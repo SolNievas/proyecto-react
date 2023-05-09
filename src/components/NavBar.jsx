db241c2e1f1ee7750eadd83909c03e9e6fd1271f
SolNievas <sol.nievas@hotmail.com>
2023-05-08 21:23:16 -0300

    E-COMMERCE

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div className="container-fluid">
            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Tienda Eyewear</a>
+                    <a className="navbar-brand" href="#">TIENDA EYEWEAR</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" 
                     data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
